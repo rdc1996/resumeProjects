4753613af21c84244a09ce7e1751e2e032957b7f
Ryan C <36077108+rdc1996@users.noreply.github.com>
2021-07-28 01:32:27 -0400

    IOS Style Calculator in Visual Basic

---
+++ IOS Calculator/Form1.Designer.vb
@@ -0,0 +1,284 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.num1 = New System.Windows.Forms.Button()
+        Me.PrintDialog1 = New System.Windows.Forms.PrintDialog()
+        Me.num2 = New System.Windows.Forms.Button()
+        Me.num3 = New System.Windows.Forms.Button()
+        Me.num4 = New System.Windows.Forms.Button()
+        Me.num5 = New System.Windows.Forms.Button()
+        Me.num6 = New System.Windows.Forms.Button()
+        Me.num7 = New System.Windows.Forms.Button()
+        Me.num8 = New System.Windows.Forms.Button()
+        Me.num9 = New System.Windows.Forms.Button()
+        Me.num0 = New System.Windows.Forms.Button()
+        Me.textBox = New System.Windows.Forms.TextBox()
+        Me.decimalBtn = New System.Windows.Forms.Button()
+        Me.clearBtn = New System.Windows.Forms.Button()
+        Me.negateBtn = New System.Windows.Forms.Button()
+        Me.percentBtn = New System.Windows.Forms.Button()
+        Me.divisionBtn = New System.Windows.Forms.Button()
+        Me.multiplyBtn = New System.Windows.Forms.Button()
+        Me.subtractBtn = New System.Windows.Forms.Button()
+        Me.addBtn = New System.Windows.Forms.Button()
+        Me.equalBtn = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'num1
+        '
+        Me.num1.Location = New System.Drawing.Point(66, 179)
+        Me.num1.Name = "num1"
+        Me.num1.Size = New System.Drawing.Size(72, 52)
+        Me.num1.TabIndex = 0
+        Me.num1.Text = "1"
+        Me.num1.UseVisualStyleBackColor = True
+        '
+        'PrintDialog1
+        '
+        Me.PrintDialog1.UseEXDialog = True
+        '
+        'num2
+        '
+        Me.num2.Location = New System.Drawing.Point(144, 179)
+        Me.num2.Name = "num2"
+        Me.num2.Size = New System.Drawing.Size(72, 52)
+        Me.num2.TabIndex = 1
+        Me.num2.Text = "2"
+        Me.num2.UseVisualStyleBackColor = True
+        '
+        'num3
+        '
+        Me.num3.Location = New System.Drawing.Point(222, 179)
+        Me.num3.Name = "num3"
+        Me.num3.Size = New System.Drawing.Size(72, 52)
+        Me.num3.TabIndex = 2
+        Me.num3.Text = "3"
+        Me.num3.UseVisualStyleBackColor = True
+        '
+        'num4
+        '
+        Me.num4.Location = New System.Drawing.Point(66, 237)
+        Me.num4.Name = "num4"
+        Me.num4.Size = New System.Drawing.Size(72, 52)
+        Me.num4.TabIndex = 3
+        Me.num4.Text = "4"
+        Me.num4.UseVisualStyleBackColor = True
+        '
+        'num5
+        '
+        Me.num5.Location = New System.Drawing.Point(144, 237)
+        Me.num5.Name = "num5"
+        Me.num5.Size = New System.Drawing.Size(72, 52)
+        Me.num5.TabIndex = 4
+        Me.num5.Text = "5"
+        Me.num5.UseVisualStyleBackColor = True
+        '
+        'num6
+        '
+        Me.num6.Location = New System.Drawing.Point(222, 237)
+        Me.num6.Name = "num6"
+        Me.num6.Size = New System.Drawing.Size(72, 52)
+        Me.num6.TabIndex = 5
+        Me.num6.Text = "6"
+        Me.num6.UseVisualStyleBackColor = True
+        '
+        'num7
+        '
+        Me.num7.Location = New System.Drawing.Point(66, 295)
+        Me.num7.Name = "num7"
+        Me.num7.Size = New System.Drawing.Size(72, 52)
+        Me.num7.TabIndex = 6
+        Me.num7.Text = "7"
+        Me.num7.UseVisualStyleBackColor = True
+        '
+        'num8
+        '
+        Me.num8.Location = New System.Drawing.Point(144, 295)
+        Me.num8.Name = "num8"
+        Me.num8.Size = New System.Drawing.Size(72, 52)
+        Me.num8.TabIndex = 7
+        Me.num8.Text = "8"
+        Me.num8.UseVisualStyleBackColor = True
+        '
+        'num9
+        '
+        Me.num9.Location = New System.Drawing.Point(222, 295)
+        Me.num9.Name = "num9"
+        Me.num9.Size = New System.Drawing.Size(72, 52)
+        Me.num9.TabIndex = 8
+        Me.num9.Text = "9"
+        Me.num9.UseVisualStyleBackColor = True
+        '
+        'num0
+        '
+        Me.num0.Location = New System.Drawing.Point(66, 353)
+        Me.num0.Name = "num0"
+        Me.num0.Size = New System.Drawing.Size(150, 52)
+        Me.num0.TabIndex = 9
+        Me.num0.Text = "0"
+        Me.num0.UseVisualStyleBackColor = True
+        '
+        'textBox
+        '
+        Me.textBox.Font = New System.Drawing.Font("Segoe UI", 20.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.textBox.Location = New System.Drawing.Point(66, 63)
+        Me.textBox.Name = "textBox"
+        Me.textBox.Size = New System.Drawing.Size(305, 52)
+        Me.textBox.TabIndex = 10
+        '
+        'decimalBtn
+        '
+        Me.decimalBtn.Location = New System.Drawing.Point(222, 353)
+        Me.decimalBtn.Name = "decimalBtn"
+        Me.decimalBtn.Size = New System.Drawing.Size(71, 51)
+        Me.decimalBtn.TabIndex = 11
+        Me.decimalBtn.Text = "."
+        Me.decimalBtn.UseVisualStyleBackColor = True
+        '
+        'clearBtn
+        '
+        Me.clearBtn.Location = New System.Drawing.Point(66, 121)
+        Me.clearBtn.Name = "clearBtn"
+        Me.clearBtn.Size = New System.Drawing.Size(72, 52)
+        Me.clearBtn.TabIndex = 12
+        Me.clearBtn.Text = "AC"
+        Me.clearBtn.UseVisualStyleBackColor = True
+        '
+        'negateBtn
+        '
+        Me.negateBtn.Location = New System.Drawing.Point(144, 121)
+        Me.negateBtn.Name = "negateBtn"
+        Me.negateBtn.Size = New System.Drawing.Size(72, 52)
+        Me.negateBtn.TabIndex = 13
+        Me.negateBtn.Text = "+/-"
+        Me.negateBtn.UseVisualStyleBackColor = True
+        '
+        'percentBtn
+        '
+        Me.percentBtn.Location = New System.Drawing.Point(221, 121)
+        Me.percentBtn.Name = "percentBtn"
+        Me.percentBtn.Size = New System.Drawing.Size(72, 52)
+        Me.percentBtn.TabIndex = 14
+        Me.percentBtn.Text = "%"
+        Me.percentBtn.UseVisualStyleBackColor = True
+        '
+        'divisionBtn
+        '
+        Me.divisionBtn.Location = New System.Drawing.Point(299, 121)
+        Me.divisionBtn.Name = "divisionBtn"
+        Me.divisionBtn.Size = New System.Drawing.Size(72, 52)
+        Me.divisionBtn.TabIndex = 15
+        Me.divisionBtn.Text = "/"
+        Me.divisionBtn.UseVisualStyleBackColor = True
+        '
+        'multiplyBtn
+        '
+        Me.multiplyBtn.Location = New System.Drawing.Point(300, 179)
+        Me.multiplyBtn.Name = "multiplyBtn"
+        Me.multiplyBtn.Size = New System.Drawing.Size(72, 52)
+        Me.multiplyBtn.TabIndex = 16
+        Me.multiplyBtn.Text = "X"
+        Me.multiplyBtn.UseVisualStyleBackColor = True
+        '
+        'subtractBtn
+        '
+        Me.subtractBtn.Location = New System.Drawing.Point(300, 237)
+        Me.subtractBtn.Name = "subtractBtn"
+        Me.subtractBtn.Size = New System.Drawing.Size(72, 52)
+        Me.subtractBtn.TabIndex = 17
+        Me.subtractBtn.Text = "-"
+        Me.subtractBtn.UseVisualStyleBackColor = True
+        '
+        'addBtn
+        '
+        Me.addBtn.Location = New System.Drawing.Point(300, 295)
+        Me.addBtn.Name = "addBtn"
+        Me.addBtn.Size = New System.Drawing.Size(72, 52)
+        Me.addBtn.TabIndex = 18
+        Me.addBtn.Text = "+"
+        Me.addBtn.UseVisualStyleBackColor = True
+        '
+        'equalBtn
+        '
+        Me.equalBtn.Location = New System.Drawing.Point(300, 353)
+        Me.equalBtn.Name = "equalBtn"
+        Me.equalBtn.Size = New System.Drawing.Size(72, 51)
+        Me.equalBtn.TabIndex = 19
+        Me.equalBtn.Text = "="
+        Me.equalBtn.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(444, 460)
+        Me.Controls.Add(Me.equalBtn)
+        Me.Controls.Add(Me.addBtn)
+        Me.Controls.Add(Me.subtractBtn)
+        Me.Controls.Add(Me.multiplyBtn)
+        Me.Controls.Add(Me.divisionBtn)
+        Me.Controls.Add(Me.percentBtn)
+        Me.Controls.Add(Me.negateBtn)
+        Me.Controls.Add(Me.clearBtn)
+        Me.Controls.Add(Me.decimalBtn)
+        Me.Controls.Add(Me.textBox)
+        Me.Controls.Add(Me.num0)
+        Me.Controls.Add(Me.num9)
+        Me.Controls.Add(Me.num8)
+        Me.Controls.Add(Me.num7)
+        Me.Controls.Add(Me.num6)
+        Me.Controls.Add(Me.num5)
+        Me.Controls.Add(Me.num4)
+        Me.Controls.Add(Me.num3)
+        Me.Controls.Add(Me.num2)
+        Me.Controls.Add(Me.num1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents num1 As Button
+    Friend WithEvents PrintDialog1 As PrintDialog
+    Friend WithEvents num2 As Button
+    Friend WithEvents num3 As Button
+    Friend WithEvents num4 As Button
+    Friend WithEvents num5 As Button
+    Friend WithEvents num6 As Button
+    Friend WithEvents num7 As Button
+    Friend WithEvents num8 As Button
+    Friend WithEvents num9 As Button
+    Friend WithEvents num0 As Button
+    Friend WithEvents textBox As TextBox
+    Friend WithEvents decimalBtn As Button
+    Friend WithEvents clearBtn As Button
+    Friend WithEvents negateBtn As Button
+    Friend WithEvents percentBtn As Button
+    Friend WithEvents divisionBtn As Button
+    Friend WithEvents multiplyBtn As Button
+    Friend WithEvents subtractBtn As Button
+    Friend WithEvents addBtn As Button
+    Friend WithEvents equalBtn As Button
+End Class
---
+++ IOS Calculator/Form1.vb
@@ -0,0 +1,319 @@
+﻿Public Class Form1
+
+    Private isPercent As Boolean = False
+    Private isBeingChanged As Boolean = False
+    Private equalsClicked As Boolean = False
+    Private operand As String = ""
+    Private numbers(2) As Double
+    Private colorButtons = New Button() {divisionBtn, multiplyBtn, subtractBtn, addBtn}
+
+    Private Sub num1_Click(sender As Object, e As EventArgs) Handles num1.Click
+
+        textBox.Text = textBox.Text + "1"
+        If isPercent Or isBeingChanged Then
+            textBox.Text = "1"
+            isPercent = False
+            isBeingChanged = False
+        End If
+        equalsClicked = False
+
+    End Sub
+
+    Private Sub num2_Click(sender As Object, e As EventArgs) Handles num2.Click
+
+        textBox.Text = textBox.Text + "2"
+        If isPercent Or isBeingChanged Then
+            textBox.Text = "2"
+            isPercent = False
+            isBeingChanged = False
+        End If
+        equalsClicked = False
+
+    End Sub
+
+    Private Sub num3_Click(sender As Object, e As EventArgs) Handles num3.Click
+
+        textBox.Text = textBox.Text + "3"
+        If isPercent Or isBeingChanged Then
+            textBox.Text = "3"
+            isPercent = False
+            isBeingChanged = False
+        End If
+        equalsClicked = False
+
+    End Sub
+
+    Private Sub num4_Click(sender As Object, e As EventArgs) Handles num4.Click
+
+        textBox.Text = textBox.Text + "4"
+        If isPercent Or isBeingChanged Then
+            textBox.Text = "4"
+            isPercent = False
+            isBeingChanged = False
+        End If
+        equalsClicked = False
+
+    End Sub
+
+    Private Sub num5_Click(sender As Object, e As EventArgs) Handles num5.Click
+
+        textBox.Text = textBox.Text + "5"
+        If isPercent Or isBeingChanged Then
+            textBox.Text = "5"
+            isPercent = False
+            isBeingChanged = False
+        End If
+        equalsClicked = False
+
+    End Sub
+
+    Private Sub num6_Click(sender As Object, e As EventArgs) Handles num6.Click
+
+        textBox.Text = textBox.Text + "6"
+        If isPercent Or isBeingChanged Then
+            textBox.Text = "6"
+            isPercent = False
+            isBeingChanged = False
+        End If
+        equalsClicked = False
+
+    End Sub
+
+    Private Sub num7_Click(sender As Object, e As EventArgs) Handles num7.Click
+
+        textBox.Text = textBox.Text + "7"
+        If isPercent Or isBeingChanged Then
+            textBox.Text = "7"
+            isPercent = False
+            isBeingChanged = False
+        End If
+        equalsClicked = False
+
+    End Sub
+
+    Private Sub num8_Click(sender As Object, e As EventArgs) Handles num8.Click
+
+        textBox.Text = textBox.Text + "8"
+        If isPercent Or isBeingChanged Then
+            textBox.Text = "8"
+            isPercent = False
+            isBeingChanged = False
+        End If
+        equalsClicked = False
+
+    End Sub
+
+    Private Sub num9_Click(sender As Object, e As EventArgs) Handles num9.Click
+
+        textBox.Text = textBox.Text + "9"
+        If isPercent Or isBeingChanged Then
+            textBox.Text = "9"
+            isPercent = False
+            isBeingChanged = False
+        End If
+        equalsClicked = False
+
+    End Sub
+
+    Private Sub num0_Click(sender As Object, e As EventArgs) Handles num0.Click
+
+        textBox.Text = textBox.Text + "0"
+        If isPercent Or isBeingChanged Then
+            textBox.Text = "0"
+            isPercent = False
+            isBeingChanged = False
+        End If
+        equalsClicked = False
+
+    End Sub
+
+    Private Sub decimalBtn_Click(sender As Object, e As EventArgs) Handles decimalBtn.Click
+
+        textBox.Text = textBox.Text + "."
+        equalsClicked = False
+
+    End Sub
+
+    Private Sub clearBtn_Click(sender As Object, e As EventArgs) Handles clearBtn.Click
+
+        textBox.Text = ""
+        equalsClicked = False
+        resetNumbers()
+
+    End Sub
+
+    Private Sub negateBtn_Click(sender As Object, e As EventArgs) Handles negateBtn.Click
+
+        Dim finalText As String = textBox.Text
+        Dim negated As String = finalText.Substring(0, 1)
+        If negated = "-" Then
+            finalText = finalText.Substring(1)
+        Else
+            finalText = "-" + finalText
+        End If
+
+        textBox.Text = finalText
+        equalsClicked = False
+
+    End Sub
+
+    Private Sub percentBtn_Click(sender As Object, e As EventArgs) Handles percentBtn.Click
+
+        Dim finalText As String = textBox.Text
+        If Len(finalText) = 0 Then
+            finalText = finalText
+        ElseIf Len(finalText) = 1 Then
+            finalText = "0.0" + finalText
+        ElseIf Len(finalText) = 2 Then
+            finalText = "0." + finalText
+        Else
+            Dim beginning As String = finalText.Substring(0, Len(finalText) - 2)
+            Dim ending As String = finalText.Substring(Len(finalText) - 2)
+            finalText = beginning + "." + ending
+        End If
+
+        textBox.Text = finalText
+        isPercent = True
+        equalsClicked = False
+
+    End Sub
+
+    Private Sub divisionBtn_Click(sender As Object, e As EventArgs) Handles divisionBtn.Click
+
+        'changeButtonColor(divisionBtn, Color.White)
+        If numbers(0) = 0 Then
+            numbers(0) = textBox.Text
+        Else
+            numbers(1) = textBox.Text
+            secondEqualsMethod()
+        End If
+        operand = "/"
+        isBeingChanged = True
+        equalsClicked = False
+
+    End Sub
+
+    Private Sub multiplyBtn_Click(sender As Object, e As EventArgs) Handles multiplyBtn.Click
+
+        'changeButtonColor(divisionBtn, Color.White)
+        If numbers(0) = 0 Then
+            numbers(0) = textBox.Text
+        Else
+            numbers(1) = textBox.Text
+            secondEqualsMethod()
+        End If
+        isBeingChanged = True
+        operand = "X"
+        equalsClicked = False
+
+    End Sub
+
+    Private Sub subtractBtn_Click(sender As Object, e As EventArgs) Handles subtractBtn.Click
+
+        'changeButtonColor(divisionBtn, Color.White)
+        If numbers(0) = 0 Then
+            numbers(0) = textBox.Text
+        Else
+            numbers(1) = textBox.Text
+            secondEqualsMethod()
+        End If
+        isBeingChanged = True
+        operand = "-"
+        equalsClicked = False
+
+    End Sub
+
+    Private Sub addBtn_Click(sender As Object, e As EventArgs) Handles addBtn.Click
+
+        'changeButtonColor(divisionBtn, Color.White)
+        If numbers(0) = 0 Then
+            numbers(0) = textBox.Text
+        Else
+            numbers(1) = textBox.Text
+            secondEqualsMethod()
+        End If
+        operand = "+"
+        isBeingChanged = True
+        equalsClicked = False
+
+    End Sub
+
+    Private Sub equalBtn_Click(sender As Object, e As EventArgs) Handles equalBtn.Click
+
+        'resetButtonColor()
+        Dim firstNum As Double = numbers(0)
+        Dim secondNum As Double = textBox.Text
+        If equalsClicked Then
+            secondNum = numbers(1)
+        End If
+        Dim finalAnswer As Double = 0
+        If operand = "/" Then
+            finalAnswer = firstNum / secondNum
+        ElseIf operand = "X" Then
+            finalAnswer = firstNum * secondNum
+        ElseIf operand = "-" Then
+            finalAnswer = firstNum - secondNum
+        Else
+            finalAnswer = firstNum + secondNum
+        End If
+
+        textBox.Text = finalAnswer
+        numbers(1) = secondNum
+        numbers(0) = finalAnswer
+        equalsClicked = True
+
+    End Sub
+
+    Private Sub secondEqualsMethod()
+
+        Dim firstNum As Double = numbers(0)
+        Dim secondNum As Double = textBox.Text
+        If equalsClicked Then
+            secondNum = numbers(1)
+        End If
+        Dim finalAnswer As Double = 0
+        If operand = "/" Then
+            finalAnswer = firstNum / secondNum
+        ElseIf operand = "X" Then
+            finalAnswer = firstNum * secondNum
+        ElseIf operand = "-" Then
+            finalAnswer = firstNum - secondNum
+        Else
+            finalAnswer = firstNum + secondNum
+        End If
+
+        textBox.Text = finalAnswer
+        numbers(1) = secondNum
+        numbers(0) = finalAnswer
+        equalsClicked = True
+
+    End Sub
+
+    Private Sub resetNumbers()
+        For number = 0 To 2
+            numbers(number) = 0
+        Next
+    End Sub
+
+    Private Sub changeButtonColor(ByVal button As Button, ByVal color As Color)
+
+        button.BackColor = color
+
+    End Sub
+
+    Private Sub resetButtonColor()
+        For Each btn As Button In colorButtons
+            btn.BackColor = Color.Transparent
+        Next
+    End Sub
+
+    Private Sub resetButtonColorExceptThis(ByVal button As Button)
+        For Each btn As Button In colorButtons
+            If btn.Equals(button) Then
+                Continue For
+            End If
+            btn.BackColor = Color.Transparent
+        Next
+    End Sub
+
+End Class
